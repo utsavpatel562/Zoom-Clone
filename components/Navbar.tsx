1a3adee94fabb239846e0ffac1774be7ef283574
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-09 19:01:57 -0400

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/icons/logo.svg"
+            width={32}
+            height={32}
+            className="max-sm:size-10"
+            alt="Zoom Clone"
+          />
+          <p className="text-[26px] font-extrabold text-white max-sm:hidden ">
+            Zoom Clone
+          </p>
+        </Link>
+        <div className="flex-between gap-5">
+          {/*Clerk - User Management*/}
+          <MobileNav />
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
