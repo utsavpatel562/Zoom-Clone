0e518ba3e14bded6cf9964237952ea05bc4ff858
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-11 14:35:25 -0400

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 
 const Home = () => {
+  const now = new Date();
+
+  const time = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+    now
+  );
   return (
     <>
       <section className="flex size-full flex-col gap-10 text-white">
@@ -10,8 +19,10 @@ const Home = () => {
               Upcoming Meeting at: 12:30 PM
             </h2>
             <div className="flex flex-col gap-2 ">
-              <h1>11:30 AM</h1>
-              <p>Saturday, March 23, 2024</p>
+              <h1 className="text-4xl font-extrabold lg:text-6xl">{time}</h1>
+              <p className="text-lg font-medium text-sky-1 lg:text-2xl">
+                {date}
+              </p>
             </div>
           </div>
         </div>
