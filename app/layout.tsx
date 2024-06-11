739cbe69601b4d9b5fac49a3ddec23857acb9ab0
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-11 12:10:48 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,21 @@ export default function RootLayout({
   return (
     <>
       <html lang="en">
-        <ClerkProvider>
+        <ClerkProvider
+          appearance={{
+            layout: {
+              logoImageUrl: "/icons/logo.svg",
+              socialButtonsVariant: "iconButton",
+            },
+            variables: {
+              colorText: "#fff",
+              colorPrimary: "#0E78F9",
+              colorBackground: "#1c1f2e",
+              colorInputBackground: "#252a41",
+              colorInputText: "#fff",
+            },
+          }}
+        >
           <body className={`${inter.className} bg-dark-2`}>{children}</body>
         </ClerkProvider>
       </html>
