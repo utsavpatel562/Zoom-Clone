cf513ac00265249bf07799769abe499dd74f420c
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-12 16:49:20 -0400

    Add files via upload

---
+++ components/HomeCard.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import React from "react";
+
+const HomeCard = () => {
+  return (
+    <>
+      <div
+        className="bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px]
+        rounded-[14px] cursor-pointer"
+        onClick={() => {}}
+      >
+        <div className="flex-center glassmorphism size-12 rounded-[10px]">
+          <Image
+            src="/icons/add-meeting.svg"
+            alt="meeting"
+            width={27}
+            height={27}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <h1 className="text-xl font-bold">New Meeting</h1>
+          <p className="text-lg font-normal">Start an instant meeting</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomeCard;
