46dd9bf4b574a84353d946ca281d0df9e6ae25cf
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-12 17:16:25 -0400

    Update MeetingTypeList.tsx

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,13 +1,38 @@
 "use client";
-import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import HomeCard from "./HomeCard";
 
 const MeetingTypeList = () => {
+  const [meetingState, setMeetingState] = useState<
+    "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
+  >();
   return (
     <>
       <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
-        <HomeCard />
+        <HomeCard
+          img="/icons/add-meeting.svg"
+          title="New Meeting"
+          description="Start an instant meeting"
+          handleClick={() => setMeetingState("isJoiningMeeting")}
+        />
+        <HomeCard
+          img="/icons/schedule.svg"
+          title="Schedule Meeting"
+          description="Plan your meeting"
+          handleClick={() => setMeetingState("isScheduleMeeting")}
+        />
+        <HomeCard
+          img="/icons/recordings.svg"
+          title="View Recording"
+          description="Check out your recordings"
+          handleClick={() => setMeetingState("isJoiningMeeting")}
+        />
+        <HomeCard
+          img="/icons/add-meeting.svg"
+          title="New Meeting"
+          description="Start an instant meeting"
+          handleClick={() => setMeetingState("isJoiningMeeting")}
+        />
       </section>
     </>
   );
