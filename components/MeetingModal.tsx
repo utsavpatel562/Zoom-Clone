39443b37d265a5ce980cc84afeea66c8b2334e51
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-13 17:34:22 -0400

    Update MeetingModal.tsx

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -1,4 +1,14 @@
 import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import Image from "next/image";
+
 interface MeetingModalProps {
   isOpen: boolean;
   onClose: () => void;
@@ -10,6 +20,7 @@ interface MeetingModalProps {
   image?: string;
   buttonIcon?: string;
 }
+
 const MeetingModal = ({
   isOpen,
   onClose,
@@ -21,7 +32,22 @@ const MeetingModal = ({
   image,
   buttonIcon,
 }: MeetingModalProps) => {
-  return <></>;
+  return (
+    <>
+      <Dialog open={isOpen} onOpenChange={onClose}>
+        <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
+          <div className="flex flex-col gap-6">
+            {image && (
+              <div className="flex justify-center">
+                <Image src={image} alt="Image" width={72} height={72} />
+              </div>
+            )}
+            <h1>{title}</h1>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
 };
 
 export default MeetingModal;
