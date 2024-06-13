eb41edddc9a55917820371bda599465759ee1f43
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-13 11:38:02 -0400

    Update HomeCard.tsx

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -1,12 +1,28 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
+interface HomeCardProps {
+  className: string;
+  img: string;
+  title: string;
+  description: string;
+  handleClick: () => void;
+}
 
-const HomeCard = () => {
+const HomeCard = ({
+  className,
+  img,
+  title,
+  description,
+  handleClick,
+}: HomeCardProps) => {
   return (
     <>
       <div
-        className="bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px]
-        rounded-[14px] cursor-pointer"
+        className={cn(
+          "px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer",
+          className
+        )}
         onClick={() => {}}
       >
         <div className="flex-center glassmorphism size-12 rounded-[10px]">
