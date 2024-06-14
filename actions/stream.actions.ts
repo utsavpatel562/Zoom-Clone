8ea1334e0712ba21436772368982a006c83f1261
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-14 12:51:45 -0400

    Add files via upload

---
+++ actions/stream.actions.ts
@@ -0,0 +1,14 @@
+"use server"
+
+import { currentUser } from "@clerk/nextjs/server";
+import { StreamClient } from "@stream-io/node-sdk";
+
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+const apiSecret = process.env.STREAM_SECRET_KEY;
+export const tokenProvider = async () =>{
+    const user = await currentUser();
+    if(!user) throw new Error("User is not logged in");
+    if(!apiKey) throw new Error("No API Key");
+    if(!apiSecret) throw new Error("No API Secret");
+    const client = new StreamClient(apiKey, apiSecret);
+}
