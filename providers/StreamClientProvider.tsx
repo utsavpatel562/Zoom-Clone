f26bc6939202460f698ef9dce93930b41b736bc0
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-14 21:13:56 -0400

    Update StreamClientProvider.tsx

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -1,3 +1,5 @@
+import { tokenProvider } from "@/actions/stream.actions";
+import Loader from "@/components/Loader";
 import { useUser } from "@clerk/nextjs";
 import { StreamVideoClient, StreamVideo } from "@stream-io/video-react-sdk";
 import { ReactNode, useEffect, useState } from "react";
@@ -17,8 +19,11 @@ const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
         name: user?.username || user?.id,
         image: user?.imageUrl,
       },
+      tokenProvider,
     });
+    setVideoClient(client);
   }, [user, isLoaded]);
-  return <StreamVideo client={videoClient}></StreamVideo>;
+  if (!videoClient) return <Loader />;
+  return <StreamVideo client={videoClient}>{children}</StreamVideo>;
 };
 export default StreamVideoProvider;
