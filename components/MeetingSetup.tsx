3320f219e569b2430fc755d1884807e52d2aeb3c
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-17 19:41:28 -0400

    Update MeetingSetup.tsx

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+"use client";
+
+import { VideoPreview, useCall } from "@stream-io/video-react-sdk";
+import React, { useEffect, useState } from "react";
 
 const MeetingSetup = () => {
-  return <div>MeetingSetup</div>;
+  const [isMicCamToggledOn, setIsMicCamToggleOn] = useState(false);
+  useEffect(() => {
+    if(isMicCamToggledOn)
+
+    const call = useCall();
+  }, [isMicCamToggledOn, call?.camera, call.microphone]);
+  return (
+    <>
+      <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+        <h1 className="text-2xl font-bold">Setup</h1>
+        <VideoPreview />
+      </div>
+    </>
+  );
 };
 
 export default MeetingSetup;
