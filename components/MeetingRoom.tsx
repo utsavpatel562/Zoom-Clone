aff43b0a2df507394380d146418861db15d35481
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-18 14:03:17 -0400

    Update MeetingRoom.tsx

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -14,15 +14,30 @@ import {
   CallControls,
   CallParticipantsList,
   CallStatsButton,
+  CallingState,
   PaginatedGridLayout,
   SpeakerLayout,
+  useCallStateHooks,
 } from "@stream-io/video-react-sdk";
 import React, { useState } from "react";
-import { LayoutList } from "lucide-react";
+import { LayoutList, Users } from "lucide-react";
+import { useSearchParams } from "next/navigation";
+import EndCallButton from "./EndCallButton";
+import Loader from "./Loader";
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
+
 const MeetingRoom = () => {
+  const searchParams = useSearchParams();
+  const isPersonalRoom = !!searchParams.get("personal");
+
   const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
   const [showParticipants, setShowParticipants] = useState(false);
+
+  const { useCallCallingState } = useCallStateHooks();
+  const callingState = useCallCallingState();
+
+  if (callingState !== CallingState.JOINED) return;
+  <Loader />;
 
   const CallLayout = () => {
     switch (layout) {
@@ -50,7 +65,7 @@ const MeetingRoom = () => {
             <CallParticipantsList onClose={() => setShowParticipants(false)} />
           </div>
         </div>
-        <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 ">
+        <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap">
           <CallControls />
           <DropdownMenu>
             <div className="flex items-center">
@@ -77,8 +92,11 @@ const MeetingRoom = () => {
           </DropdownMenu>
           <CallStatsButton />
           <button onClick={() => setShowParticipants((prev) => !prev)}>
-            <div className="cursor-pointer rounded-2xl"></div>
+            <div className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+              <Users size={20} className="text-white" />
+            </div>
           </button>
+          {!isPersonalRoom && <EndCallButton />}
         </div>
       </section>
     </>
