74518da9a9a3e146f9e26e4f6b939c9a1d412a32
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-17 21:32:36 -0400

    Update MeetingRoom.tsx

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,7 +1,37 @@
-import React from "react";
-
+import {
+  Call,
+  PaginatedGridLayout,
+  SpeakerLayout,
+} from "@stream-io/video-react-sdk";
+import React, { useState } from "react";
+type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 const MeetingRoom = () => {
-  return <div>MeetingRoom</div>;
+  const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
+  const [showParticipants, setShowParticipants] = useState(false);
+
+  const CallLayout = () => {
+    switch (layout) {
+      case "grid":
+        return <PaginatedGridLayout />;
+      case "speaker-right":
+        return <SpeakerLayout participantsBarPosition={"left"} />;
+      default:
+        return <SpeakerLayout participantsBarPosition={"right"} />;
+    }
+  };
+
+  return (
+    <>
+      <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+        <div className="relative flex size-full items-center justify-center">
+          <div className="flex size-full max-w-[1000px] items-center">
+            <CallLayout />
+          </div>
+          <div className="h-[calc(100vh-86px)] hidden ml-2"></div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default MeetingRoom;
