6c65cfd03cc1b50880e8a96c823b59d7647b088b
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-19 13:28:39 -0400

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,14 @@
 import StreamVideoProvider from "@/providers/StreamClientProvider";
 import React, { ReactNode } from "react";
 
+export const metadata: Metadata = {
+  title: "ZOOM CLONE (UTSAV PATEL)",
+  description: "Video Calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <>
