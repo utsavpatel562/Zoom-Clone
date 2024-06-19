a5fcea6f9744af17b45c096fe33036f67dd7f6d7
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-19 13:28:52 -0400

    Update layout.tsx

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -2,6 +2,14 @@ import Navbar from "@/components/Navbar";
 import { Sidebar } from "lucide-react";
 import React, { ReactNode } from "react";
 
+export const metadata: Metadata = {
+  title: "ZOOM CLONE (UTSAV PATEL)",
+  description: "Video Calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
 const HomeLayout = ({ children }: { children: ReactNode }) => {
   return (
     <>
