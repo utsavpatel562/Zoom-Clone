7d172cb846339e5e7b0d368e2c704879c6e3de7b
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-20 17:52:11 -0400

    Update page.tsx

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Recordings = () => {
-  return <div>Recordings</div>;
+  return (
+    <>
+      <section className="flex size-full flex-col gap-10 text-white">
+        <h1 className="text-3xl font-bold">Recordings</h1>
+        <CallList type="recordings" />
+      </section>
+    </>
+  );
 };
 
 export default Recordings;
