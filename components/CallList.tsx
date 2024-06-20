9e9bfed0bc74ed2642e0eb5065c15d4cb8aaa7f3
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-20 08:56:37 -0400

    Update CallList.tsx

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,6 +1,40 @@
-import React from "react";
+"use client";
+
+import { useGetCalls } from "@/hooks/useGetCalls";
+import { CallRecording } from "@stream-io/video-react-sdk";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
+  const { endedCalls, upcomingCalls, callRecordings, isLoading } =
+    useGetCalls();
+  const router = useRouter();
+
+  const [recordings, setRecordings] = useState<CallRecording[]>([]);
+  const getCalls = () => {
+    switch (type) {
+      case "ended":
+        return endedCalls;
+      case "recordings":
+        return recordings;
+      case "upcoming":
+        return upcomingCalls;
+      default:
+        return [];
+    }
+  };
+  const getNoCallsMessage = () => {
+    switch (type) {
+      case "ended":
+        return "No Previous Calls";
+      case "recordings":
+        return "No Recordings";
+      case "upcoming":
+        return "No Upcoming Meetings";
+      default:
+        return "";
+    }
+  };
   return <div>CallList</div>;
 };
 
