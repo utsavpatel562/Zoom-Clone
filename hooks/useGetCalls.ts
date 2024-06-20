1a57a28598fdf887cf52263618793ab69b0abd3a
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-19 21:43:52 -0400

    Add files via upload

---
+++ hooks/useGetCalls.ts
@@ -0,0 +1,53 @@
+import { useUser } from "@clerk/nextjs";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk"
+import { useEffect, useState } from "react"
+import { newDate } from "react-datepicker/dist/date_utils";
+
+export const useGetCalls = ()=>{
+    const [calls, setCalls] = useState<Call[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const client = useStreamVideoClient();
+    const {user} = useUser();
+
+    useEffect(()=>{
+        const loadCalls = async()=>{
+            if(!client || !user?.id) return;
+            setIsLoading(true);
+
+            try {
+                const {calls} = await client.queryCalls({
+                    sort: [{field: 'starts_at', direction:-1}],
+                    filter_conditions: {
+                        starts_at: {$exists: true},
+                        $or: [
+                            {createad_by_user_id: user.id},
+                            {members: {$in: [user.id]}}
+                        ]
+                    }
+                });
+                setCalls(calls);
+            } catch (error) {
+                console.log(error);
+            } finally{
+                setIsLoading(false);
+            }
+        }
+        loadCalls();
+    }, [client, user?.id])
+   
+    const now = newDate();
+
+    const endedCalls = calls.filter(({state:{startsAt, endedAt}}: Call)=>{
+        return (startsAt && new Date(startsAt) < now || !!endedAt)
+    });
+    const upcomingCalls = calls.filter(({state:{startsAt}}: Call)=>{
+        return startsAt && new Date(startsAt) > now
+    })
+   
+    return {
+     endedCalls,
+     upcomingCalls,
+    recordings: calls,
+    isLoading,
+    }
+}
