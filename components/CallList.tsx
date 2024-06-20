608019051c2eeabc319e4b757c3d94955143ae0c
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-20 18:12:17 -0400

    Update CallList.tsx

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,52 +1,118 @@
-import { useEffect, useState } from 'react';
-import { useUser } from '@clerk/nextjs';
-import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
+"use client";
 
-export const useGetCalls = () => {
-  const { user } = useUser();
-  const client = useStreamVideoClient();
-  const [calls, setCalls] = useState<Call[]>();
-  const [isLoading, setIsLoading] = useState(false);
+import { Call, CallRecording } from "@stream-io/video-react-sdk";
+
+import Loader from "./Loader";
+import { useGetCalls } from "@/hooks/useGetCalls";
+import MeetingCard from "./MeetingCard";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useToast } from "./ui/use-toast";
+
+const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
+  const router = useRouter();
+  const { endedCalls, upcomingCalls, callRecordings, isLoading } =
+    useGetCalls();
+  const [recordings, setRecordings] = useState<CallRecording[]>([]);
+  const { toast } = useToast();
+
+  const getCalls = () => {
+    switch (type) {
+      case "ended":
+        return endedCalls;
+      case "recordings":
+        return recordings;
+      case "upcoming":
+        return upcomingCalls;
+      default:
+        return [];
+    }
+  };
+
+  const getNoCallsMessage = () => {
+    switch (type) {
+      case "ended":
+        return "No Previous Calls";
+      case "upcoming":
+        return "No Upcoming Calls";
+      case "recordings":
+        return "No Recordings";
+      default:
+        return "";
+    }
+  };
 
   useEffect(() => {
-    const loadCalls = async () => {
-      if (!client || !user?.id) return;
-      
-      setIsLoading(true);
-
+    const fetchRecordings = async () => {
       try {
-        // https://getstream.io/video/docs/react/guides/querying-calls/#filters
-        const { calls } = await client.queryCalls({
-          sort: [{ field: 'starts_at', direction: -1 }],
-          filter_conditions: {
-            starts_at: { $exists: true },
-            $or: [
-              { created_by_user_id: user.id },
-              { members: { $in: [user.id] } },
-            ],
-          },
-        });
+        const callData = await Promise.all(
+          callRecordings?.map((meeting) => meeting.queryRecordings()) ?? []
+        );
 
-        setCalls(calls);
+        const recordings = callData
+          .filter((call) => call.recordings.length > 0)
+          .flatMap((call) => call.recordings);
+
+        setRecordings(recordings);
       } catch (error) {
-        console.error(error);
-      } finally {
-        setIsLoading(false);
+        toast({ title: "Try again later" });
       }
     };
 
-    loadCalls();
-  }, [client, user?.id]);
+    if (type === "recordings") {
+      fetchRecordings();
+    }
+  }, [type, callRecordings]);
 
-  const now = new Date();
+  if (isLoading) return <Loader />;
 
-  const endedCalls = calls?.filter(({ state: { startsAt, endedAt } }: Call) => {
-    return (startsAt && new Date(startsAt) < now) || !!endedAt
-  })
+  const calls = getCalls();
+  const noCallsMessage = getNoCallsMessage();
 
-  const upcomingCalls = calls?.filter(({ state: { startsAt } }: Call) => {
-    return startsAt && new Date(startsAt) > now
-  })
-
-  return { endedCalls, upcomingCalls, callRecordings: calls, isLoading }
+  return (
+    <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
+      {calls && calls.length > 0 ? (
+        calls.map((meeting: Call | CallRecording) => (
+          <MeetingCard
+            key={(meeting as Call).id}
+            icon={
+              type === "ended"
+                ? "/icons/previous.svg"
+                : type === "upcoming"
+                ? "/icons/upcoming.svg"
+                : "/icons/recordings.svg"
+            }
+            title={
+              (meeting as Call).state?.custom?.description ||
+              (meeting as CallRecording).filename?.substring(0, 20) ||
+              "No Description"
+            }
+            date={
+              (meeting as Call).state?.startsAt?.toLocaleString() ||
+              (meeting as CallRecording).start_time?.toLocaleString()
+            }
+            isPreviousMeeting={type === "ended"}
+            link={
+              type === "recordings"
+                ? (meeting as CallRecording).url
+                : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${
+                    (meeting as Call).id
+                  }`
+            }
+            buttonIcon1={type === "recordings" ? "/icons/play.svg" : undefined}
+            buttonText={type === "recordings" ? "Play" : "Start"}
+            handleClick={
+              type === "recordings"
+                ? () => router.push(`${(meeting as CallRecording).url}`)
+                : () => router.push(`/meeting/${(meeting as Call).id}`)
+            }
+          />
+        ))
+      ) : (
+        <h1 className="text-2xl font-bold text-white">{noCallsMessage}</h1>
+      )}
+    </div>
+  );
 };
+
+export default CallList;
