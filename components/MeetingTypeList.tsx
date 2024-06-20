20d1cb299965d67967f42ec434eb51b831ef8cee
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-20 18:59:14 -0400

    Update MeetingTypeList.tsx

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -10,6 +10,7 @@ import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 import { useToast } from "./ui/use-toast";
 import { Textarea } from "./ui/textarea";
 import ReactDatePicker from "react-datepicker";
+import { Input } from "./ui/input";
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -90,7 +91,7 @@ const MeetingTypeList = () => {
           img="/icons/recordings.svg"
           title="View Recording"
           description="Check out your recordings"
-          handleClick={() => setMeetingState("isJoiningMeeting")}
+          handleClick={() => router.push("/recordings")}
           className="bg-purple-1"
         />
         <HomeCard
@@ -159,6 +160,20 @@ const MeetingTypeList = () => {
           buttonText="Start Meeting"
           handleClick={createMeeting}
         />
+        <MeetingModal
+          isOpen={meetingState === "isJoiningMeeting"}
+          onClose={() => setMeetingState(undefined)}
+          title="Type the link here"
+          className="text-center"
+          buttonText="Join Meeting"
+          handleClick={() => router.push(values.link)}
+        >
+          <Input
+            placeholder="Meeting Link"
+            className="border-none bg-dark-3 focus-visible: ring-0 focus-visible: ring-offset-0"
+            onChange={(e) => setValues({ ...values, link: e.target.value })}
+          />
+        </MeetingModal>
       </section>
     </>
   );
