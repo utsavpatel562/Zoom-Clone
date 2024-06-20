4ffad383a236d762f1bdf1c67eaf17a27a22bd52
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-20 09:13:36 -0400

    Update CallList.tsx

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { useGetCalls } from "@/hooks/useGetCalls";
-import { CallRecording } from "@stream-io/video-react-sdk";
+import { Call, CallRecording } from "@stream-io/video-react-sdk";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
+import MeetingCard from "./MeetingCard";
 
 const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
   const { endedCalls, upcomingCalls, callRecordings, isLoading } =
@@ -35,7 +36,17 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
         return "";
     }
   };
-  return <div>CallList</div>;
+  const calls = getCalls();
+  const noCallsMessage = getNoCallsMessage();
+  return (
+    <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
+      {calls && calls.length > 0 ? (
+        calls.map((meeting: Call | CallRecording) => <MeetingCard />)
+      ) : (
+        <h1>{noCallsMessage}</h1>
+      )}
+    </div>
+  );
 };
 
 export default CallList;
