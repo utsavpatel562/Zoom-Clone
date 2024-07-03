78813c0bb9cddc151cd772cbce06a8fb2b9aca9d
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-07-03 14:39:14 -0400

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,4 +1,5 @@
 import MeetingTypeList from "@/components/MeetingTypeList";
+import Upcoming from "./upcoming/page";
 
 const Home = () => {
   const now = new Date();
@@ -12,21 +13,28 @@ const Home = () => {
   );
 
   return (
-    <section className="flex size-full flex-col gap-5 text-white">
-      <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
-        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-            Upcoming Meeting at: 12:30 PM
-          </h2>
-          <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
-            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+    <>
+      <section className="flex size-full flex-col gap-5 text-white">
+        <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
+          <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+            <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
+              Upcoming Meeting at: 12:30 PM
+            </h2>
+            <div className="flex flex-col gap-2">
+              <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+              <p className="text-lg font-medium text-sky-1 lg:text-2xl">
+                {date}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <MeetingTypeList />
-    </section>
+        <MeetingTypeList />
+        <div className="mt-5">
+          <Upcoming />
+        </div>
+      </section>
+    </>
   );
 };
 
